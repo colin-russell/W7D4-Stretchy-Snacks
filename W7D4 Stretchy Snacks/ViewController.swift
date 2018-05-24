abfcda403543863902e0ef2db6c03d70a1587693
Colin Russell <colinrussell101@gmail.com>
2018-05-24 08:12:07 -0400

    Initial Commit

---
+++ W7D4 Stretchy Snacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W7D4 Stretchy Snacks
+//
+//  Created by Colin on 2018-05-24.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
